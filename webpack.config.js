b5234fad5a8815827631a61fd44bc65b27cf4ce3
judysome <judysome@gmail.com>
2022-02-06 15:41:08 -0700

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const webpack = require("webpack");
-
-// module.exports = (env) => {
-//     // Use env.<YOUR VARIABLE> here:
-//     // console.log("Goal: ", env.goal); // 'local'
-//     // console.log("Production: ", env.production); // true
-
-//     return {
-//         // mode: "development",
-//         entry: "./src/index.js",
-//         output: {
-//             filename: "bundle.js",
-//             path: path.resolve(__dirname, "dist"),
-//         },
-//         // plugins: [new HtmlWebpackPlugin({ template: "./dist/index.html" })],
-//         devServer: {
-//             contentBase: "/dist",
-//             // hot: true,
-//         },
-//     };
-// };
 
 var config = {
     mode: "development",
@@ -30,20 +9,8 @@ var config = {
         path: path.resolve(__dirname, "dist"),
     },
     plugins: [new HtmlWebpackPlugin({ template: "./dist/index.html" })],
-    // devServer: {
-    //     contentBase: "/dist",
-    //     // hot: true,
-    // },
 };
 
-module.exports = (env, argv) => {
-    if (argv.mode === "development") {
-        config.devtool = "source-map";
-    }
-
-    if (argv.mode === "production") {
-        //...
-    }
-
+module.exports = () => {
     return config;
 };
