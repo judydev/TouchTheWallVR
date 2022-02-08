a23a8002bfbcf863917d939e75e7b35d0eb7ffd2
judysome <judysome@gmail.com>
2022-02-07 21:16:03 -0700

    add lib.js

---
+++ src/utils/lib.js
@@ -0,0 +1,8 @@
+export function makeButtonMesh(x, y, z, color) {
+    const geometry = new THREE.BoxGeometry(x, y, z);
+    const material = new THREE.MeshPhongMaterial({ color: color });
+    const buttonMesh = new THREE.Mesh(geometry, material);
+    buttonMesh.castShadow = true;
+    buttonMesh.receiveShadow = true;
+    return buttonMesh;
+}
